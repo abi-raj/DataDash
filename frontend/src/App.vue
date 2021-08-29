156ee03ecaba55a4bb5d16880c1d181e46cefc24
abiraj-rajendran <abiraj.rajendran@zoho.com>
2021-08-29 12:29:56 +0530

    base files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <form action="
+  ">
+  <input type="file"/>
+  </form>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>

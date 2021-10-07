00b1c1aefc09cea142afa401aa28a3c68731c427
abiraj-rajendran <abiraj.rajendran@zoho.com>
2021-10-07 19:14:05 +0530

    basic prediction

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
 from flask import Flask,request,jsonify
 import pandas as pd
-app = Flask(__name__)
+from flask_cors import CORS
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+CORS(app)
+
+
 
 #functions
 def dtype(df,cols):
@@ -42,6 +49,24 @@ def upload():
         for cname in lis:
           result[cname]=ndf[cname].tolist()
         return jsonify(result)
+
+@app.route('/pred',methods=['POST'])
+def pred():
+  data=request.get_json()
+  
+  # x=[1,2,3,4,5,6,7]
+  # y=[2,3,4,5,6,7,8]
+  x=data["x"]
+  # print(x)
+  y=data["y"]
+  target=data["target"]
+  xx=np.array(x).reshape((-1,1))
+  yy=np.array(y).reshape((-1,1))
+  # print(target)
+  regr = LinearRegression()
+  regr.fit(xx,yy)
+  y_pred=regr.predict([[target]])
+  return jsonify({"result":y_pred[0][0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
